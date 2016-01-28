e1c148809e5a5da4e0b6d7413c9e825a40bcc061
Douglas Alipio Mesquita <d.alipio@sidiamao.com.br>
2016-01-28 10:51:54 -0400

    Created main script nad touchpad listen

--- gvrf_accessibility_sample/src/com/samsung/accessibility/MainActivity.java
+++ gvrf_accessibility_sample/src/com/samsung/accessibility/MainActivity.java
@@ -1,16 +1,37 @@
 
 package com.samsung.accessibility;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import org.gearvrf.GVRActivity;
 
-public class MainActivity extends Activity {
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+import com.samsung.accessibility.VRTouchPadGestureDetector.OnTouchPadGestureListener;
+import com.samsung.accessibility.VRTouchPadGestureDetector.SwipeDirection;
+
+public class MainActivity extends GVRActivity implements OnTouchPadGestureListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setScript(new MainScript(), "gvr.xml");
+    }
+
+    @Override
+    public boolean onSingleTap(MotionEvent e) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public boolean onSwipe(MotionEvent e, SwipeDirection swipeDirection, float velocityX, float velocityY) {
+        // TODO Auto-generated method stub
+        return false;
     }
 }
---
+++ gvrf_accessibility_sample/src/com/samsung/accessibility/MainScript.java
@@ -0,0 +1,21 @@
+
+package com.samsung.accessibility;
+
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRScript;
+
+public class MainScript extends GVRScript {
+
+    @Override
+    public void onInit(GVRContext gvrContext) throws Throwable {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStep() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
