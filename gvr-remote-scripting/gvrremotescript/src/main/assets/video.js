3bce26517eb579cc2aa691e57bccc6d37419b6f8
thomasflynn <tom.flynn@samsung.com>
2016-04-28 11:26:10 -0700

    update url in video.js
    url changed with the move to AS.  updating
    
    
    GearVRf-DCO-1.0-Signed-off-by: Tom Flynn
    tom.flynn@samsung.com

--- gvr-remote-scripting/gvrremotescript/src/main/assets/video.js
+++ gvr-remote-scripting/gvrremotescript/src/main/assets/video.js
@@ -3,7 +3,7 @@ importPackage(org.gearvrf.scene_objects);
 
 var sphere = new GVRSphereSceneObject(gvrf, false);
 var mesh = sphere.getRenderData().getMesh();
-var mediaPlayer = new org.gearvrf.utility.ImageUtils.createMediaPlayer("https://raw.githubusercontent.com/gearvrf/GearVRf-Demos/master/gvrf_minimal360video/assets/videos_s_3.mp4");
+var mediaPlayer = new org.gearvrf.utility.ImageUtils.createMediaPlayer("https://github.com/gearvrf/GearVRf-Demos/raw/master/gvr-360video/gvr360video/src/main/assets/videos_s_3.mp4");
 var video = new GVRVideoSceneObject(gvrf, mesh, mediaPlayer, GVRVideoSceneObject.GVRVideoType.MONO );
 video.setName("video");
 var scene = gvrf.getMainScene();
