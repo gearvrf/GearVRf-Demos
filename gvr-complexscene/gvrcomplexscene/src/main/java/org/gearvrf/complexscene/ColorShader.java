36dd6d41674b5149247087f4a345fc6f1dd99a27
Nola Donato <nola.donato@samsung.com>
2016-05-12 18:41:24 -0700

    reverted to single light version compatible with older versions of GearVRF

--- gvr-complexscene/gvrcomplexscene/src/main/java/org/gearvrf/complexscene/ColorShader.java
+++ gvr-complexscene/gvrcomplexscene/src/main/java/org/gearvrf/complexscene/ColorShader.java
@@ -1,29 +1,38 @@
 package org.gearvrf.complexscene;
 
 import org.gearvrf.GVRContext;
-import org.gearvrf.GVRShaderTemplate;
+import org.gearvrf.GVRCustomMaterialShaderId;
+import org.gearvrf.GVRMaterialMap;
+import org.gearvrf.GVRMaterialShaderManager;
 
-public class ColorShader extends GVRShaderTemplate {
+public class ColorShader {
 
-    private static final String VERTEX_SHADER = "in vec3 a_position;\n"
+    public static final String COLOR_KEY = "color";
+
+    private static final String VERTEX_SHADER = "attribute vec4 a_position;\n"
             + "uniform mat4 u_mvp;\n" //
             + "void main() {\n" //
-            + "  gl_Position = u_mvp * vec4(a_position, 1.0);\n" //
+            + "  gl_Position = u_mvp * a_position;\n" //
             + "}\n";
 
     private static final String FRAGMENT_SHADER = "precision mediump float;\n"
-            + "uniform vec4 u_color;\n"
-            + "out vec4 gl_FragColor;\n"
+            + "uniform vec4 u_color;\n" //
             + "void main() {\n" //
             + "  gl_FragColor = u_color;\n" //
             + "}\n";
 
+    private GVRCustomMaterialShaderId mShaderId;
+    private GVRMaterialMap mCustomShader = null;
 
-    public ColorShader(GVRContext gvrcontext)
-    {
-        super("float4 u_color");
-        setSegment("FragmentTemplate", FRAGMENT_SHADER);
-        setSegment("VertexTemplate", VERTEX_SHADER);
+    public ColorShader(GVRContext gvrContext) {
+        final GVRMaterialShaderManager shaderManager = gvrContext
+                .getMaterialShaderManager();
+        mShaderId = shaderManager.addShader(VERTEX_SHADER, FRAGMENT_SHADER);
+        mCustomShader = shaderManager.getShaderMap(mShaderId);
+        mCustomShader.addUniformVec4Key("u_color", COLOR_KEY);
     }
 
+    public GVRCustomMaterialShaderId getShaderId() {
+        return mShaderId;
+    }
 }
