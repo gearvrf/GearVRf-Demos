f6fec4376677aa5733142a95b68ca6fd050d07fd
Deepak Singh Rawat <deepakrawat2287@gmail.com>
2016-07-01 10:44:40 -0700

    Viewport from config file

--- gvr-vuforia/app/src/main/java/org/gearvrf/vuforiasample/VuforiaSampleMain.java
+++ gvr-vuforia/app/src/main/java/org/gearvrf/vuforiasample/VuforiaSampleMain.java
@@ -66,8 +66,6 @@ public class VuforiaSampleMain extends GVRMain {
         this.gvrContext = gvrContext;
         mainScene = gvrContext.getMainScene();
         mainScene.getMainCameraRig().setFarClippingDistance(20000);
-        // viewport as per default 1024 x 1024 window size
-        mainScene.getMainCameraRig().setViewport(0, 224, 1024, 576);
 
         createTeaPotObject();
 
@@ -210,9 +208,6 @@ public class VuforiaSampleMain extends GVRMain {
 
     public void updateObjectPose() {
         State state = mRenderer.begin();
-
-        // viewport as per default 1024 x 1024 window size
-        mainScene.getMainCameraRig().setViewport(0, 224, 1024, 576);
 
         // did we find any trackables this frame?
         int numDetectedMarkers = state.getNumTrackableResults();
