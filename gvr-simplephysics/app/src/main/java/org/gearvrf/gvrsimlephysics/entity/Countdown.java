b272e5e5723dae193c8580bf3d7c3499093affae
Ragner Magalhaes <ragner.magalhaes@gmail.com>
2016-12-01 18:41:12 -0400

    New bullet simple demo (#374)
    * First commit.
    
    GearVRf-DCO-1.0-Signed-off-by: Douglas A Mesquita <d.alipio@samsung.com>
    
    * Improves game logic
    
    GearVRf-DCO-1.0-Signed-off-by: Ragner Magalhaes <ragner.n@samsung.com>
    
    * Add UX doc
    
    GearVRf-DCO-1.0-Signed-off-by: Ragner Magalhaes <ragner.n@samsung.com>

---
+++ gvr-simplephysics/app/src/main/java/org/gearvrf/gvrsimlephysics/entity/Countdown.java
@@ -0,0 +1,68 @@
+package org.gearvrf.gvrsimlephysics.entity;
+
+import android.os.CountDownTimer;
+
+import org.gearvrf.GVRContext;
+import org.gearvrf.scene_objects.GVRTextViewSceneObject;
+
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by d.alipio@samsung.com on 11/4/16.
+ */
+
+public class Countdown {
+    private GVRTextViewSceneObject timeObject;
+    private static final String FORMAT = "%02d:%02d";
+    private boolean mIsFinished;
+    private CountDownTimer mCountDownTimer;
+
+    public Countdown(GVRTextViewSceneObject label) {
+        timeObject = label;
+        mIsFinished = false;
+        mCountDownTimer = null;
+    }
+
+    public void start(GVRContext context) {
+        mIsFinished = false;
+        context.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mCountDownTimer = new CountDownTimer(120000, 1000) {
+
+                    public void onTick(long millisUntilFinished) {
+
+                        long minutes = TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) -
+                                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millisUntilFinished));
+                        long seconds = TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
+                                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished));
+
+                        timeObject.setText("" + String.format(Locale.ENGLISH, FORMAT, minutes, seconds));
+                    }
+
+                    public void onFinish() {
+                        timeObject.setText("timeout!");
+                        mIsFinished = true;
+                    }
+
+                };
+                mCountDownTimer.start();
+            }
+        });
+    }
+
+    public boolean isFinished() {
+        return mIsFinished;
+    }
+
+    public void stop(GVRContext context) {
+        mIsFinished = true;
+        context.getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mCountDownTimer.cancel();
+            }
+        });
+    }
+}
