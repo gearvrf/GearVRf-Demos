61112c49a9e6b8431148adf99c7de974ec3fe550
Mihail Marinov <m.marinov@samsung.com>
2017-04-18 15:26:29 -0700

    Fix gvr-accessibility

--- gvr-accessibility/app/src/main/java/com/samsung/accessibility/util/AccessibilityManager.java
+++ gvr-accessibility/app/src/main/java/com/samsung/accessibility/util/AccessibilityManager.java
@@ -10,24 +10,23 @@
  */
 package com.samsung.accessibility.util;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.gearvrf.GVRContext;
-import org.gearvrf.accessibility.GVRAccessibilityInvertedColors;
 import org.gearvrf.accessibility.GVRAccessibilityTalkBack;
 import org.gearvrf.accessibility.GVRAccessibilityZoom;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccessibilityManager {
 
     private List<GVRAccessibilityTalkBack> mTalkBacks;
 
-    private GVRAccessibilityInvertedColors mInvertedColors;
+    private AccessibilityInvertedColors mInvertedColors;
     private GVRAccessibilityZoom mZoom;
 
     public AccessibilityManager(GVRContext gvrContext) {
         mTalkBacks = new ArrayList<GVRAccessibilityTalkBack>();
-        mInvertedColors = new GVRAccessibilityInvertedColors(gvrContext);
+        mInvertedColors = new AccessibilityInvertedColors(gvrContext);
         mZoom = new GVRAccessibilityZoom();
     }
 
@@ -35,7 +34,7 @@ public class AccessibilityManager {
         return mTalkBacks;
     }
 
-    public GVRAccessibilityInvertedColors getInvertedColors() {
+    public AccessibilityInvertedColors getInvertedColors() {
         return mInvertedColors;
     }
 
