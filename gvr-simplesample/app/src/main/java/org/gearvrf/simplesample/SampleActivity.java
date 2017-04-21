5c4c2089fe6ac715b618e793cb4516af0200bffd
Mihail Marinov <m.marinov@samsung.com>
2017-04-21 09:25:50 -0700

    gvr-simplesample: make it more compact
    scale the logo also; remove unused image

--- gvr-simplesample/app/src/main/java/org/gearvrf/simplesample/SampleActivity.java
+++ gvr-simplesample/app/src/main/java/org/gearvrf/simplesample/SampleActivity.java
@@ -15,9 +15,16 @@
 
 package org.gearvrf.simplesample;
 
-import org.gearvrf.GVRActivity;
-
 import android.os.Bundle;
+
+import org.gearvrf.GVRActivity;
+import org.gearvrf.GVRAndroidResource;
+import org.gearvrf.GVRCameraRig;
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRMain;
+import org.gearvrf.GVRScene;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRTexture;
 
 public class SampleActivity extends GVRActivity {
 
@@ -25,5 +32,25 @@ public class SampleActivity extends GVRActivity {
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setMain(new SampleMain());
+    }
+
+    private static class SampleMain extends GVRMain {
+        @Override
+        public void onInit(GVRContext gvrContext) {
+            GVRScene scene = gvrContext.getMainScene();
+            scene.setBackgroundColor(1, 1, 1, 1);
+
+            GVRTexture texture = gvrContext.getAssetLoader().loadTexture(new GVRAndroidResource(gvrContext, R.drawable.gearvr_logo));
+
+            // create a scene object (this constructor creates a rectangular scene
+            // object that uses the standard texture shader
+            GVRSceneObject sceneObject = new GVRSceneObject(gvrContext, 4.0f, 2.0f, texture);
+
+            // set the scene object position
+            sceneObject.getTransform().setPosition(0.0f, 0.0f, -3.0f);
+
+            // add the scene object to the scene graph
+            scene.addSceneObject(sceneObject);
+        }
     }
 }
