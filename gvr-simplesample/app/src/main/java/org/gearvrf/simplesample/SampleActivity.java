28b8d172572ee3d9fe8ae1f1b57319be67b725ec
Nola Donato <nola.donato@samsung.com>
2017-05-03 14:02:23 -0700

    merge from master

--- gvr-simplesample/app/src/main/java/org/gearvrf/simplesample/SampleActivity.java
+++ gvr-simplesample/app/src/main/java/org/gearvrf/simplesample/SampleActivity.java
@@ -15,15 +15,41 @@
 
 package org.gearvrf.simplesample;
 
-import org.gearvrf.GVRActivity;
-
 import android.os.Bundle;
+
+import org.gearvrf.GVRActivity;
+import org.gearvrf.GVRAndroidResource;
+import org.gearvrf.GVRContext;
+import org.gearvrf.GVRMain;
+import org.gearvrf.GVRScene;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.GVRTexture;
 
 public class SampleActivity extends GVRActivity {
 
     @Override
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        setMain(new SampleMain(), "gvr.xml");
+        setMain(new SampleMain());
+    }
+
+    private static class SampleMain extends GVRMain {
+        @Override
+        public void onInit(GVRContext gvrContext) {
+            GVRScene scene = gvrContext.getMainScene();
+            scene.setBackgroundColor(1, 1, 1, 1);
+
+            GVRTexture texture = gvrContext.getAssetLoader().loadTexture(new GVRAndroidResource(gvrContext, R.drawable.gearvr_logo));
+
+            // create a scene object (this constructor creates a rectangular scene
+            // object that uses the standard texture shader
+            GVRSceneObject sceneObject = new GVRSceneObject(gvrContext, 4.0f, 2.0f, texture);
+
+            // set the scene object position
+            sceneObject.getTransform().setPosition(0.0f, 0.0f, -3.0f);
+
+            // add the scene object to the scene graph
+            scene.addSceneObject(sceneObject);
+        }
     }
 }
