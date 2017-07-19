47391a5bab9ccac5a548e7994659cd1ebf18ef8c
nola.donato@samsung.com <nola.donato@samsung.com>
2017-07-19 16:35:53 -0700

    gvr-javascript fixed custom shader version, gvr-cardboardaudio fix lights
    1. Don't use lighting in gvr-cardboardaudio if running on Vulkan
    2. gvr-cardboardaudio - Use GVRDirectLight instead of GVRLight
    3. gvr-meshanimation - use GVRAnimator

--- gvr-meshanimation/app/src/main/java/org/gearvrf/gvrmeshanimation/MeshAnimationMain.java
+++ gvr-meshanimation/app/src/main/java/org/gearvrf/gvrmeshanimation/MeshAnimationMain.java
@@ -1,20 +1,33 @@
 package org.gearvrf.gvrmeshanimation;
 
-import android.util.Log;
+import java.io.IOException;
 
 import org.gearvrf.GVRActivity;
 import org.gearvrf.GVRContext;
-import org.gearvrf.GVRMain;
 import org.gearvrf.GVRScene;
+import org.gearvrf.GVRMain;
+import org.gearvrf.animation.GVRAnimation;
+import org.gearvrf.animation.GVRAnimationEngine;
 import org.gearvrf.animation.GVRAnimator;
+import org.gearvrf.animation.GVRRepeatMode;
 import org.gearvrf.scene_objects.GVRModelSceneObject;
 
-import java.io.IOException;
+import android.util.Log;
 
 public class MeshAnimationMain extends GVRMain {
 
-    private GVRActivity mActivity;
+    private GVRContext mGVRContext;
     private GVRModelSceneObject mCharacter;
+
+    private final String mModelPath = "TRex_NoGround.fbx";
+
+    private GVRActivity mActivity;
+
+    private static final String TAG = "MeshAnimationSample";
+
+    private GVRAnimationEngine mAnimationEngine;
+    GVRAnimator mAssimpAnimation = null;
+    
 
     public MeshAnimationMain(GVRActivity activity) {
         mActivity = activity;
@@ -22,26 +35,32 @@ public class MeshAnimationMain extends GVRMain {
 
     @Override
     public void onInit(GVRContext gvrContext) {
-        final GVRScene nextMainScene = gvrContext.getMainScene();
+        mGVRContext = gvrContext;
+        mAnimationEngine = gvrContext.getAnimationEngine();
+
+        GVRScene mainScene = gvrContext.getMainScene();
+
 
         try {
-            mCharacter = gvrContext.getAssetLoader().loadModel(mModelPath);
+            mCharacter = gvrContext.getAssetLoader().loadModel(mModelPath, mainScene);
             mCharacter.getTransform().setPosition(0.0f, -10.0f, -10.0f);
             mCharacter.getTransform().setRotationByAxis(90.0f, 1.0f, 0.0f, 0.0f);
             mCharacter.getTransform().setRotationByAxis(40.0f, 0.0f, 1.0f, 0.0f);
             mCharacter.getTransform().setScale(1.5f, 1.5f, 1.5f);
 
-            nextMainScene.addSceneObject(mCharacter);
-            final GVRAnimator animator = (GVRAnimator)mCharacter.getComponent(GVRAnimator.getComponentType());
-            animator.start();
+            mAssimpAnimation = (GVRAnimator) mCharacter.getComponent(GVRAnimator.getComponentType());
+            mAssimpAnimation.setRepeatMode(GVRRepeatMode.REPEATED);
+            mAssimpAnimation.setRepeatCount(-1);
         } catch (IOException e) {
             e.printStackTrace();
             mActivity.finish();
             mActivity = null;
             Log.e(TAG, "One or more assets could not be loaded.");
         }
+        mAssimpAnimation.start();
     }
 
-    private static final String mModelPath = "TRex_NoGround.fbx";
-    private static final String TAG = "MeshAnimationSample";
+    @Override
+    public void onStep() {
+    }
 }
