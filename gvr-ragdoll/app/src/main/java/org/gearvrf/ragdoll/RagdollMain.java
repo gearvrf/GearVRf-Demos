576cb6a0dfd46daff1b0f1d5bdd49024f492e313
Ragner Magalhaes <ragner.n@samsung.com>
2018-06-22 11:43:42 -0400

    ragdoll-demo: Loading physics file
    * Loads the fbx file that has the graphical scene objects
    * Load the bullet file that has all rag doll  physics.
    
    GearVRf-DCO-1.0-Signed-off-by: Ragner Magalhaes <ragner.n@samsung.com>

--- gvr-ragdoll/app/src/main/java/org/gearvrf/ragdoll/RagdollMain.java
+++ gvr-ragdoll/app/src/main/java/org/gearvrf/ragdoll/RagdollMain.java
@@ -15,14 +15,106 @@
 
 package org.gearvrf.ragdoll;
 
+import org.gearvrf.GVRAndroidResource;
 import org.gearvrf.GVRContext;
+import org.gearvrf.GVREventListeners;
 import org.gearvrf.GVRMain;
+import org.gearvrf.GVRPicker;
+import org.gearvrf.GVRRenderData;
+import org.gearvrf.GVRScene;
+import org.gearvrf.GVRSceneObject;
+import org.gearvrf.io.GVRCursorController;
+import org.gearvrf.io.GVRInputManager;
+import org.gearvrf.physics.GVRPhysicsLoader;
+import org.gearvrf.physics.GVRRigidBody;
+import org.gearvrf.physics.GVRWorld;
+import org.gearvrf.utility.Log;
+
+import java.util.EnumSet;
 
 public class RagdollMain extends GVRMain {
+    private final static String TAG = "RagDoll";
+    private final static float CURSOR_DEPTH = 6.0f;
+
+    private GVRCursorController mCursorController = null;
+    private GVRSceneObject mCursor = null;
+    private TouchHandler mTouchHandler = null;
+
     public RagdollMain() {}
 
     @Override
-    public void onInit(GVRContext gvrContext) throws Throwable {
+    public void onInit(final GVRContext gvrContext) throws Throwable {
         super.onInit(gvrContext);
+
+        final GVRScene scene = gvrContext.getMainScene();
+
+        scene.setBackgroundColor(0,0,0,0);
+
+        Log.d(TAG, "Loading Rag Doll mesh...");
+        GVRSceneObject model = gvrContext.getAssetLoader().loadModel("models/ragdoll.fbx", scene);
+
+        model.getTransform().setPosition(0,0, -3);
+        scene.addSceneObject(model);
+
+        GVRWorld world = new GVRWorld(gvrContext);
+        world.setGravity(0f, -1f, 0f);
+        scene.getRoot().attachComponent(world);
+
+        Log.d(TAG, "Loading Rag Doll physics...");
+        GVRPhysicsLoader.loadPhysicsFile(gvrContext,
+                "models/ragdoll.bullet", true, scene);
+
+        initCursorController(gvrContext);
+    }
+
+    /**
+     * Initialize GearVR controller handler.
+     *
+     * @param gvrContext GVRf context.
+     */
+    private void initCursorController(GVRContext gvrContext) {
+        GVRScene scene = gvrContext.getMainScene();
+        mTouchHandler = new TouchHandler();
+
+        scene.getEventReceiver().addListener(mTouchHandler);
+        GVRInputManager inputManager = gvrContext.getInputManager();
+        mCursor = new GVRSceneObject(gvrContext,
+                gvrContext.createQuad(0.2f * CURSOR_DEPTH,
+                        0.2f * CURSOR_DEPTH),
+                gvrContext.getAssetLoader().loadTexture(new GVRAndroidResource(gvrContext,
+                        R.raw.cursor)));
+        mCursor.getRenderData().setDepthTest(false);
+        mCursor.getRenderData().setRenderingOrder(GVRRenderData.GVRRenderingOrder.OVERLAY);
+        final EnumSet<GVRPicker.EventOptions> eventOptions = EnumSet.of(
+                GVRPicker.EventOptions.SEND_TOUCH_EVENTS,
+                GVRPicker.EventOptions.SEND_TO_LISTENERS);
+        inputManager.selectController(new GVRInputManager.ICursorControllerSelectListener()
+        {
+            public void onCursorControllerSelected(GVRCursorController newController, GVRCursorController oldController)
+            {
+                if (oldController != null)
+                {
+                    oldController.removePickEventListener(mTouchHandler);
+                }
+                mCursorController = newController;
+                newController.addPickEventListener(mTouchHandler);
+                newController.setCursor(mCursor);
+                newController.setCursorDepth(-CURSOR_DEPTH);
+                newController.setCursorControl(GVRCursorController.CursorControl.CURSOR_CONSTANT_DEPTH);
+                newController.getPicker().setEventOptions(eventOptions);
+            }
+        });
+    }
+
+    private class TouchHandler extends GVREventListeners.TouchEvents {
+        @Override
+        public void onTouchStart(GVRSceneObject sceneObj, GVRPicker.GVRPickedObject collision) {
+            super.onTouchStart(sceneObj, collision);
+            GVRRigidBody rigidBody = (GVRRigidBody)sceneObj.getComponent(GVRRigidBody.getComponentType());
+            if (rigidBody != null) {
+                rigidBody.applyForce(0, 0, -50,
+                        collision.hitLocation[0], collision.hitLocation[1], collision.hitLocation[2]);
+            }
+        }
     }
 }
