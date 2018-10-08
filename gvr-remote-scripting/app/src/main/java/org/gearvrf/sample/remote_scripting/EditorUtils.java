5e5a017f538ffc071db875f5691dfad842428678
Ragner Magalhaes <ragner.n@samsung.com>
2018-10-08 14:51:21 -0400

    gvr-remote-scripting: Remove deprecated GVRViews
    GearVRf-DCO-1.0-Signed-off-by: Ragner Magalhaes <ragner.n@samsung.com>

--- gvr-remote-scripting/app/src/main/java/org/gearvrf/sample/remote_scripting/EditorUtils.java
+++ gvr-remote-scripting/app/src/main/java/org/gearvrf/sample/remote_scripting/EditorUtils.java
@@ -15,162 +15,49 @@
 
 package org.gearvrf.sample.remote_scripting;
 
+import android.graphics.Color;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import org.gearvrf.GVRContext;
-
-import org.gearvrf.scene_objects.GVRViewSceneObject;
-import org.gearvrf.scene_objects.view.GVRFrameLayout;
-import org.gearvrf.GVRSensor;
-import org.gearvrf.SensorEvent;
-import org.gearvrf.ISensorEvents;
-
+import org.gearvrf.IViewEvents;
 import org.gearvrf.debug.cli.LineProcessor;
+import org.gearvrf.scene_objects.GVRViewSceneObject;
 import org.gearvrf.script.GVRScriptManager;
 
 import java.io.StringWriter;
+
 import javax.script.ScriptContext;
 import javax.script.ScriptEngine;
 import javax.script.ScriptException;
 
-import java.lang.Runnable;
-import java.util.List;
-
-import android.graphics.Color;
-import android.os.Handler;
-import android.os.Message;
-import android.os.SystemClock;
-import android.view.View;
-import android.view.InputDevice;
-import android.view.KeyEvent;
-import android.view.MotionEvent;
-import android.view.MotionEvent.PointerCoords;
-import android.view.MotionEvent.PointerProperties;
-import android.widget.TextView;
-import android.widget.EditText;
-import android.text.Editable;
-import android.text.TextWatcher;
-
 public class EditorUtils {
-    private GVRContext gvrContext;
-    private boolean inflated = false;
-    private GVRViewSceneObject layoutSceneObject;
+    private final GVRContext gvrContext;
+    private final GVRViewSceneObject layoutSceneObject;
     private GearVRScripting activity;
-    private GVRFrameLayout frameLayout;
-    private int frameWidth;
-    private int frameHeight;
-    private Handler mainThreadHandler;
-    private final static PointerProperties[] pointerProperties;
-    private final static PointerCoords[] pointerCoordsArray;
-    private final static PointerCoords pointerCoords;
 
     private static final float QUAD_X = 2.0f;
     private static final float QUAD_Y = 1.0f;
-    private static final float HALF_QUAD_X = QUAD_X / 2.0f;
-    private static final float HALF_QUAD_Y = QUAD_Y / 2.0f;
-    private static final float DEPTH = -1.5f;
-    private ScriptHandler mScriptHandler;
+    private final ScriptHandler mScriptHandler;
 
     private TextView updateButton;
 
-    static {
-        PointerProperties properties = new PointerProperties();
-        properties.id = 0;
-        properties.toolType = MotionEvent.TOOL_TYPE_MOUSE;
-        pointerProperties = new PointerProperties[] { properties };
-        pointerCoords = new PointerCoords();
-        pointerCoordsArray = new PointerCoords[] { pointerCoords };
-    }
 
     public EditorUtils(GVRContext context) {
         gvrContext = context;
         activity = (GearVRScripting) context.getActivity();
-    }
 
-    public void inflate() {
-        mScriptHandler = new ScriptHandler(gvrContext);
-        frameLayout = new GVRFrameLayout(activity.getGVRApplication());
-        frameLayout.setDrawingCacheEnabled(false);
-        View.inflate(activity, R.layout.main, frameLayout);
-
-        final EditText editor = (EditText) frameLayout.findViewById(R.id.editor);
-        editor.requestFocus();
-        editor.setDrawingCacheEnabled(false);
-        editor.setBackgroundColor(Color.BLACK);
-        editor.addTextChangedListener(new TextWatcher() {
-                @Override
-                public void onTextChanged(CharSequence s, int start, int before, int count) {
-                }
-
-                @Override
-                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                }
-
-                @Override
-                public void afterTextChanged(Editable s) {
-                    editor.invalidate();
-                    frameLayout.invalidate();
-                    frameLayout.requestLayout();
-                }
-
-            });
-
-
-        updateButton = (TextView) frameLayout.findViewById(R.id.update);
-        updateButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    android.util.Log.d("Editor", "update was clicked");
-                    // get text
-                    String script = editor.getText().toString();
-                    // execute script
-                    mScriptHandler.processLine(script);
-                }
-            });
-
-        mainThreadHandler = new Handler(activity.getMainLooper()) {
-            @Override
-            public void handleMessage(Message msg) {
-                MotionEvent motionEvent = (MotionEvent) msg.obj;
-                frameLayout.dispatchTouchEvent(motionEvent);
-                frameLayout.invalidate();
-                frameLayout.requestLayout();
-                motionEvent.recycle();
-            }
-        };
-
-        inflated = true;
-    }
-
-    public void show() {
-        if(!inflated) {
-            activity.runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        inflate();
-                    }
-                });
-        }
-
-        while(!inflated) {
-            SystemClock.sleep(500);
-        }
-
-        if(layoutSceneObject != null) {
-            gvrContext.getMainScene().addSceneObject(layoutSceneObject);
-            return;
-        }
-
-        layoutSceneObject = new GVRViewSceneObject(gvrContext, frameLayout, gvrContext.createQuad(QUAD_X, QUAD_Y));
+        layoutSceneObject = new GVRViewSceneObject(gvrContext, R.layout.main, viewEventsHandler,
+                gvrContext.createQuad(QUAD_X, QUAD_Y));
 
         layoutSceneObject.getTransform().setPosition(0.0f, 0.0f, -1.0f);
         layoutSceneObject.setName("editor");
 
-        frameWidth = frameLayout.getWidth();
-        frameHeight = frameLayout.getHeight();
+        mScriptHandler = new ScriptHandler(gvrContext);
+    }
 
-        GVRSensor sensor = new GVRSensor(gvrContext);
-        layoutSceneObject.getEventReceiver().addListener(sensorEvents);
-        //@todo broken functionality
-        //layoutSceneObject.setSensor(sensor);
-
+    public void show() {
         gvrContext.getMainScene().addSceneObject(layoutSceneObject);
     }
 
@@ -186,45 +73,32 @@ public class EditorUtils {
         gvrContext.getMainScene().removeSceneObject(layoutSceneObject);
     }
 
-    private ISensorEvents sensorEvents = new ISensorEvents() {
-        private static final float SCALE = 1.0f;
-        private float savedMotionEventX, savedMotionEventY, savedHitPointX, savedHitPointY;
+
+    private IViewEvents viewEventsHandler = new IViewEvents() {
         @Override
-        public void onSensorEvent(final SensorEvent event) {
-            List<MotionEvent> motionEvents = event.getCursorController().getMotionEvents();
-            for (MotionEvent motionEvent : motionEvents) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_MOVE) {
-                    pointerCoords.x = savedHitPointX
-                            + ((motionEvent.getX() - savedMotionEventX) * SCALE);
-                    pointerCoords.y = savedHitPointY
-                            + ((motionEvent.getY() - savedMotionEventY) * SCALE);
-                } else {
-                    float[] hitPoint = event.getPickedObject().getHitLocation();
-                    pointerCoords.x = ((hitPoint[0] + HALF_QUAD_X) / QUAD_X) * frameWidth;
-                    pointerCoords.y = (-(hitPoint[1] - HALF_QUAD_Y) / QUAD_Y) * frameHeight;
+        public void onInitView(GVRViewSceneObject gvrViewSceneObject, View view) {
+            final EditText editor = (EditText) view.findViewById(R.id.editor);
+            editor.requestFocus();
+            editor.setDrawingCacheEnabled(false);
+            editor.setBackgroundColor(Color.BLACK);
 
-                    if (motionEvent.getAction() == KeyEvent.ACTION_DOWN) {
-                        // save the co ordinates on down
-                        savedMotionEventX = motionEvent.getX();
-                        savedMotionEventY = motionEvent.getY();
-
-                        savedHitPointX = pointerCoords.x;
-                        savedHitPointY = pointerCoords.y;
-                    }
+            updateButton = (TextView) view.findViewById(R.id.update);
+            updateButton.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View view) {
+                    android.util.Log.d("Editor", "update was clicked");
+                    // get text
+                    String script = editor.getText().toString();
+                    // execute script
+                    mScriptHandler.processLine(script);
                 }
+            });
+        }
 
-                MotionEvent clone = MotionEvent.obtain(
-                        motionEvent.getDownTime(), motionEvent.getEventTime(),
-                        motionEvent.getAction(), 1, pointerProperties,
-                        pointerCoordsArray, 0, 0, 1f, 1f, 0, 0,
-                        InputDevice.SOURCE_TOUCHSCREEN, 0);
+        @Override
+        public void onStartRendering(GVRViewSceneObject gvrViewSceneObject, View view) {
 
-                Message message = Message.obtain(mainThreadHandler, 0, 0, 0, clone);
-                mainThreadHandler.sendMessage(message);
-            }
         }
     };
-
 
     class ScriptHandler implements LineProcessor {
         protected String prompt;
